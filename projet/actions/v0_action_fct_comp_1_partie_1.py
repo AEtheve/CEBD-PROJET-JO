e2e0d3d6f3284fb5c15bf593853bb4300e667e4a
AEtheve <30652308+AEtheve@users.noreply.github.com>
2022-11-24 17:36:22 +0100

    TODO 1.1
    #2

--- projet/actions/v0_action_fct_comp_1_partie_1.py
+++ projet/actions/v0_action_fct_comp_1_partie_1.py
@@ -13,26 +13,25 @@ class AppFctComp1Partie1(QDialog):
         super(QDialog, self).__init__()
         self.ui = uic.loadUi("gui/fct_comp_1.ui", self)
         self.data = data
+        cursor = self.data.cursor()
+        result = cursor.execute("SELECT DISTINCT categorieEp FROM V0_LesEpreuves")
+        for row in result:
+            self.ui.comboBox.addItem(row[0])
 
     # Fonction de mise à jour de l'affichage
     @pyqtSlot()
     def refreshResult(self):
-        # TODO 1.1 : fonction à modifier pour remplacer la zone de saisie par une liste de valeurs prédéfinies
-        #  dans l'interface une fois le fichier ui correspondant mis à jour
         display.refreshLabel(self.ui.label_fct_comp_1, "")
-        if not self.ui.lineEdit_fct_comp_1.text().strip():
+        categorie = self.ui.comboBox.currentText()
+        try:
+            cursor = self.data.cursor()
+            result = cursor.execute(
+                "SELECT numEp, nomEp, formeEp, nomDi, categorieEp, nbSportifsEp, strftime('%Y-%m-%d',dateEp) FROM V0_LesEpreuves WHERE categorieEp = ?",
+                [categorie])
+        except Exception as e:
             self.ui.table_fct_comp_1.setRowCount(0)
-            display.refreshLabel(self.ui.label_fct_comp_1, "Veuillez indiquer un nom de catégorie")
+            display.refreshLabel(self.ui.label_fct_comp_1, "Impossible d'afficher les résultats : " + repr(e))
         else:
-            try:
-                cursor = self.data.cursor()
-                result = cursor.execute(
-                    "SELECT numEp, nomEp, formeEp, nomDi, categorieEp, nbSportifsEp, strftime('%Y-%m-%d',dateEp,'unixepoch') FROM V0_LesEpreuves WHERE categorieEp = ?",
-                    [self.ui.lineEdit_fct_comp_1.text().strip()])
-            except Exception as e:
-                self.ui.table_fct_comp_1.setRowCount(0)
-                display.refreshLabel(self.ui.label_fct_comp_1, "Impossible d'afficher les résultats : " + repr(e))
-            else:
-                i = display.refreshGenericData(self.ui.table_fct_comp_1, result)
-                if i == 0:
-                    display.refreshLabel(self.ui.label_fct_comp_1, "Aucun résultat")
+            i = display.refreshGenericData(self.ui.table_fct_comp_1, result)
+            if i == 0:
+                display.refreshLabel(self.ui.label_fct_comp_1, "Aucun résultat")
