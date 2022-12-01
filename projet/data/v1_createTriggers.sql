20104cbe19f88fecc7d333f8cd1c1272f9a4ded9
Theodora28C <theodora.ciobanu28@gmail.com>
2022-12-01 08:33:16 +0100

    Update v1_createTriggers.sql

--- projet/data/v1_createTriggers.sql
+++ projet/data/v1_createTriggers.sql
@@ -1,1 +1,33 @@
 -- TODO 3.3 Créer un trigger pertinent
+
+CREATE TRIGGER IF NOT EXISTS exaequo
+BEFORE INSERT ON MedaillesOr FOR EACH ROW
+WHEN
+(
+    SELECT numP
+    FROM MedaillesOr
+    WHERE numEp = NEW.numEp
+) IS NOT NULL
+BEGIN
+    SELECT RAISE(ABORT, 'Il y a déjà une médaille d''or pour cette épreuve');
+END
+WHEN
+(
+    SELECT numP
+    FROM MedaillesArgent
+    WHERE numEp = NEW.numEp
+) IS NOT NULL
+BEGIN
+    SELECT RAISE(ABORT, 'Il y a déjà une médaille d''argent pour cette épreuve');
+END
+WHEN
+(
+    SELECT numP
+    FROM MedaillesBronze
+    WHERE numEp = NEW.numEp
+) IS NOT NULL
+BEGIN
+    SELECT RAISE(ABORT, 'Il y a déjà une médaille de bronze pour cette épreuve');
+END;
+
+
